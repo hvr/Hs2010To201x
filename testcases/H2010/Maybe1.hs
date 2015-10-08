9a031b61295c2622ebea3baa214f6c3d568cbdd9
Alan Zimmerman <alan.zimm@gmail.com>
2015-10-08 21:04:06 +0200

    Bringing in Functor/Applicative from base for Maybe1

--- testcases/H2010/Maybe1.hs
+++ testcases/H2010/Maybe1.hs
@@ -4,6 +4,20 @@ module Maybe1 where
 
 data Maybe' a = Nothing' | Just' a
 
+instance  Functor Maybe'  where
+    fmap _ Nothing'       = Nothing'
+    fmap f (Just' a)      = Just' (f a)
+
+instance Applicative Maybe' where
+    pure = Just'
+
+    Just' f  <*> m       = fmap f m
+    Nothing' <*> _m      = Nothing'
+
+    Just' _m1 *> m2      = m2
+    Nothing'  *> _m2     = Nothing'
+
+
 instance Monad Maybe' where
     return = Just'
 
@@ -11,3 +25,4 @@ instance Monad Maybe' where
     Just' x  >>= f = f x
 
     fail _ = Nothing'
+
